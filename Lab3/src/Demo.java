b3a65d87903b0d42a6bbcedff3dc9f6058a96e4c
Aidan Harrison <aidanharrison11@gmail.com>
2023-02-07 16:59:37 -0400

    Logger creates one instance for one file

--- Lab3/src/Demo.java
+++ Lab3/src/Demo.java
@@ -1,12 +1,12 @@
 public class Demo {
     public static void main(String[] args){
-        Logger logger1 = new Logger("file 1");
-        Logger logger2 = new Logger("file 1");
+        Logger logger1 = Logger.getLogger("file 1");
+        Logger logger2 = Logger.getLogger("file 1");
 
         System.out.print("Multiple loggers can write to the same file: ");
         System.out.println(logger1 == logger2);
 
-        Logger logger3 = new Logger("file 2");
+        Logger logger3 = Logger.getLogger("file 2");
 
         System.out.print("Multiple loggers can write to different files: ");
         System.out.println(logger2 != logger3);
--- Lab3/src/Logger.java
+++ Lab3/src/Logger.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+
 public class Logger {
+    private static Logger instance;
+    private static ArrayList<String> files = new ArrayList<>();
     private String fileName;
 
-    public Logger(String fileName) {
+    private Logger(String fileName) {
+        files.add(fileName);
         this.fileName = fileName;
     }
+    public static Logger getLogger(String fileName){
+        if(!files.contains(fileName)){
+            instance = new Logger(fileName);
+        }
+        return instance;
+    }
+
     public void write(String message) {
         System.out.println("Writing a message to the log.");
     }
