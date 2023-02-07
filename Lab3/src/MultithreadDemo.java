2ad5155c10d6d2d936ea85462920f4dd798a1d32
Aidan Harrison <aidanharrison11@gmail.com>
2023-02-07 17:49:52 -0400

    Created multithread application of logger

---
+++ Lab3/src/MultithreadDemo.java
@@ -0,0 +1,35 @@
+public class MultithreadDemo {
+    public static void main(String[] args){
+        Thread t1 = new Thread(new connectionOne());
+        Thread t2 = new Thread(new connectionTwo());
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class connectionOne implements Runnable{
+
+    @Override
+    public void run() {
+        Logger logger = Logger.getLogger("Database");
+        logger.write("Connection one writes once");
+        logger.write("Connection one writes twice");
+        logger.write("Connection one writes three times");
+        System.out.println("Connection one Logger contents: ");
+        System.out.print(logger);
+    }
+}
+
+class connectionTwo implements Runnable{
+
+    @Override
+    public void run(){
+        Logger logger = Logger.getLogger("Database");
+        logger.write("Connection two writes once");
+        logger.write("Connection two writes twice");
+        logger.write("Connection two writes three times");
+        System.out.println("Connection two Logger contents: ");
+        System.out.print(logger);
+    }
+}
