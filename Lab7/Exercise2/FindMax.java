78851ffcdd8b8711db5f8fca7a762715d32c585d
Aidan Harrison <aidanharrison11@gmail.com>
2023-03-14 16:47:02 -0300

    Started Exercise 2

---
+++ Lab7/Exercise2/FindMax.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FindMax {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            list.add((int)(Math.random()*10));
+        }
+
+        System.out.println(list);
+
+        int currentMax = 0;
+        Iterator iterator = list.iterator();
+        while(iterator.hasNext()){
+            int next = iterator.
+            if(currentMax < )
+        }
+    }
+}
