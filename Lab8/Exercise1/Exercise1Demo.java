d77dbf17c79e0f48a4330903fc29794ecd9f6d2f
Aidan Harrison <aidanharrison11@gmail.com>
2023-03-21 14:50:48 -0300

    Improved readability of output

--- Lab8/Exercise1/Exercise1Demo.java
+++ Lab8/Exercise1/Exercise1Demo.java
@@ -2,22 +2,22 @@ public class Exercise1Demo {
     public static void main(String[] args){
         VideoEditor editor = new VideoEditor();
         editor.setText("Hello World");
-        System.out.println(editor);
+        System.out.println(editor + "\n");
 
         editor.undo();
-        System.out.println(editor);
+        System.out.println(editor + "\n");
 
         editor.setText("Hello World");
         editor.setContrast(0.75f);
-        System.out.println(editor);
+        System.out.println(editor + "\n");
 
         editor.undo();
-        System.out.println(editor);
+        System.out.println(editor + "\n");
 
         editor.undo();
-        System.out.println(editor);
+        System.out.println(editor + "\n");
 
         editor.undo();
-        System.out.println(editor);
+        System.out.println(editor + "\n");
     }
 }
